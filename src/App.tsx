88436ca61334f8bc8e4870d23f615a0707953925
Igor Minayeu <igor.minayeu@gmail.com>
2023-01-09 21:23:14 +0300

    button

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,27 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {MouseEvent} from 'react';
 import './App.css';
+import {Button} from "./components/Button";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const Button1Foo = (subscriber: string, age: number, address: string) => {
+        console.log(subscriber, age, address)
+    }
+    const Button2Foo = (subscriber: string, age: number) => {
+        console.log(subscriber, age)
+    }
+
+    const StupidButtonFoo = () => {
+        console.log('Im stupid button!')
+    }
+    return (
+        <div className="App">
+            {/*  <button>MyYouTubeChanel-1</button>
+            <button>MyYouTubeChanel-2</button>*/}
+            <Button name={'MyYouTubeChanel-1'} callBack={() => Button1Foo('Im Vasya', 21, 'live in Minsk')}/>
+            <Button name={'MyYouTubeChanel-2'} callBack={() => Button2Foo('Im Ivan', 23)}/>
+            <Button name={'StupidButton'} callBack={StupidButtonFoo}/>
+        </div>
+    );
 }
 
 export default App;
---
+++ src/components/Button.tsx
@@ -0,0 +1,16 @@
+import React, {FC} from 'react';
+
+type ButtonPropsType = {
+    name: string
+    callBack: () => void
+}
+
+export const Button: FC<ButtonPropsType> = (props) => {
+    const onClickHandler = () => {
+        props.callBack()
+    }
+    return (
+        <button onClick={onClickHandler}>{props.name}</button>
+    );
+};
+
